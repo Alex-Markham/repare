4c38d7b73cd035cbcd361ad5fec01bce8be12519
Alex Markham <alex.markham@causal.dev>
2025-02-01 12:20:15 +0100

    refactor: more modular oracle-based class to use as base

--- src/repare/repare.py
+++ src/repare/repare.py
@@ -2,7 +2,8 @@ import networkx as nx
 import numpy as np
 from scipy.stats import kstest
 
-class PartitionDagModel(object):
+
+class PartitionDagModelOracle(object):
     """A probabilistic graphical model in the form of a directed acyclic graph over parts of a partiton."""
 
     def __init__(self, rng=np.random.default_rng(0)) -> None:
@@ -10,45 +11,25 @@ class PartitionDagModel(object):
         self.rng = rng
 
     def fit(self, order, adj) -> object:
+        self.order = order
+        self.adj = adj
+
         # always use lexicograph order for node partitions
         self.dag.add_node(tuple(sorted(order)))  # trivial coarsening
-
-        def _is_adj(pa, ch):
-            for el_pa in pa:
-                for el_ch in ch:
-                    if el_ch in adj[el_pa]:
-                        return True
-            return False
 
         recurse = len(order) - 1
         while recurse:
             recurse -= 1
-
-            to_refine, u, v = self._refine(order)
-
-            self.dag.add_node(u)
-            self.dag.add_node(v)
-
-            for pa in self.dag.predecessors(to_refine):
-                for ch in (u, v):
-                    if _is_adj(pa, ch):
-                        self.dag.add_edge(pa, ch)
-            for pa in (u, v):
-                for ch in self.dag.successors(to_refine):
-                    if _is_adj(pa, ch):
-                        self.dag.add_edge(pa, ch)
-            if _is_adj(u, v):
-                self.dag.add_edge(u, v)
-            self.dag.remove_node(to_refine)
+            self._recurse()
         return self
 
-    def _refine(self, order):
+    def _refine(self):
         refinable_nodes = [node for node in self.dag.nodes if len(node) > 1]
         to_refine_idx = self.rng.choice(len(refinable_nodes))
         to_refine = tuple(refinable_nodes[to_refine_idx])
         u_len = self.rng.choice(range(1, len(to_refine)))  # rest go to v
         u = []
-        for el in order:
+        for el in self.order:
             if el in to_refine:
                 u.append(el)
             if len(u) == u_len:
@@ -57,6 +38,47 @@ class PartitionDagModel(object):
         v = tuple((el for el in to_refine if el not in u))
         return to_refine, u, v
 
+    def _is_adj(self, pa, ch):
+        for el_pa in pa:
+            for el_ch in ch:
+                if el_ch in self.adj[el_pa]:
+                    return True
+        return False
 
-    def fit_ks(self,data_dict):
-        return
+    def _recurse(self):
+        to_refine, u, v = self._refine()
+
+        self.dag.add_node(u)
+        self.dag.add_node(v)
+
+        for pa in self.dag.predecessors(to_refine):
+            for ch in (u, v):
+                if self._is_adj(pa, ch):
+                    self.dag.add_edge(pa, ch)
+        for pa in (u, v):
+            for ch in self.dag.successors(to_refine):
+                if self._is_adj(pa, ch):
+                    self.dag.add_edge(pa, ch)
+        if self._is_adj(u, v):
+            self.dag.add_edge(u, v)
+        self.dag.remove_node(to_refine)
+
+
+class PartitionDagModelIvn(PartitionDagModelOracle):
+    def __init__(rng=np.random.default_rng(0)) -> None:
+        super.__init__(rng)
+
+    def fit(self, data_dict, alpha=0.05):
+        obs = data_dict.pop("obs", None)  # raise error if obs is None
+        ks_results = {
+            idx: kstest(obs, post_ivn).pvalue < alpha
+            for idx, post_ivn in data_dict.items()
+        }  # <alpha means reject the null, so 0 -> 1 (in PO, not in causal dag)
+
+        ivn_biadj = {k: np.flatnonzero(v) for k, v in ks_results.items()}
+
+        # recurse
+
+    def _refine():
+        to_refine, u, v = None
+        return to_refine, u, v
--- tests/test_repare.py
+++ tests/test_repare.py
@@ -1,17 +1,17 @@
 import networkx as nx
 import numpy as np
 from numpy.random import default_rng as rng
-from repare.repare import PartitionDagModel
+from repare.repare import PartitionDagModelOracle
 from scipy.stats import kstest
 from sempler import LGANM
 from sempler.generators import dag_avg_deg, intervention_targets
 
 
-def test_fit_basic():
+def test_fit_oracle_basic():
     true_order = [3, 1, 2, 4]
     true_adj = {1: [2], 3: [2], 2: [4]}
 
-    model = PartitionDagModel()
+    model = PartitionDagModelOracle()
     model.fit(true_order, true_adj)
 
     assert len(model.dag.edges) == 3
@@ -20,7 +20,7 @@ def test_fit_basic():
             assert model.dag.has_edge((u,), (v,))
 
 
-def test_fit_extensive():
+def test_fit_oracle_extensive():
     digraph = nx.binomial_graph(100, 0.5, directed=True)
     true_order = list(rng(0).permutation(range(100)))
     s = np.argsort(true_order)
@@ -28,7 +28,7 @@ def test_fit_extensive():
     digraph.remove_edges_from(to_rm)
     true_adj = {k: [kk for kk in v.keys()] for k, v in digraph.adj.items()}
 
-    model = PartitionDagModel()
+    model = PartitionDagModelOracle()
     model.fit(true_order, true_adj)
 
 
